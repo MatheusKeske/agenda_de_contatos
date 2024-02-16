874d000eacd456d4cb7864b0807f5328a7174bf4
Matheus Keske <keskem6@gmail.com>
2024-02-16 14:59:23 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    var nome = document.getElementById('nome').value;
+    var telefone = document.getElementById('telefone').value;
+  
+    var table = document.getElementById('contactTable').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    cell1.innerHTML = nome;
+    cell2.innerHTML = telefone;
+  
+    document.getElementById('contactForm').reset();
+  });
